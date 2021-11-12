0f3a6e8a0d54942af54233e4016fd2edc96a7252
Thobias <thobiaspsilva@yahoo.com.br>
2021-11-12 02:10:14 -0300

    Adiciona messagem e usuario inicial

--- src/database.ts
+++ src/database.ts
@@ -2,5 +2,7 @@ import { v4 as uuid } from "uuid";
 import Message from "./entities/message.entity";
 import User from "./entities/user.entity";
 
-export let messagesList: Message[] = [];
-export let usersList: User[] = [new User(uuid(), "thobiassilva", "123456")];
+export let messagesList: Message[] = [
+  new Message("a", "Titulo da Mensagem A", "Descricao da Mensagem A", "a"),
+];
+export let usersList: User[] = [new User("a", "thobiassilva", "123456")];
