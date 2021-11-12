09add240860415c6e781118fbcd25b3a5d6bfe82
Thobias <thobiaspsilva@yahoo.com.br>
2021-11-12 02:12:01 -0300

    Adiciona cors, configura novas rotas, e porta

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
+import cors from "cors";
 import express from "express";
-import { routes } from "./routes";
+import { messageRoutes } from "./routes/messages.routes";
+import { authorizationValid } from "./middlewares/middlewares";
+import { routes } from "./routes/auth.routes";
 
 let app = express();
 app.use(express.json());
+app.use(cors());
+
 app.use(routes);
 
-app.listen(8081, () => console.log("Servidor iniciou"));
+app.use(authorizationValid);
+app.use("/messages", messageRoutes);
+
+app.listen(process.env.PORT || 8081, () => console.log("Server ON"));
