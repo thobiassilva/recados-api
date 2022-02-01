25ba8b630bcb0f241dcb1985a38e91359325c13f
Thobias Silva <thobiaspsilva@yahoo.com.br>
2022-01-31 23:58:26 -0300

    Cria o cacheRepository

---
+++ src/core/infra/repositories/cache.repository.ts
@@ -0,0 +1,28 @@
+import { Redis } from "ioredis";
+import { ICacheRepository } from "../../domain/contracts/cache_repository.contract";
+import { RedisConnection } from "../database/connections/redis";
+
+export class CacheRepository implements ICacheRepository {
+  private readonly redis: Redis;
+
+  constructor() {
+    this.redis = RedisConnection.getConnection();
+  }
+
+  async set(key: string, value: any): Promise<void> {
+    const result = await this.redis.set(key, JSON.stringify(value), "EX", 30);
+
+    // if (result === null) {
+    //   throw new Error("Set error");
+    // }
+  }
+
+  async get<T>(key: string): Promise<T> {
+    const result = await this.redis.get(key);
+    return result != null ? JSON.parse(result) : undefined;
+  }
+
+  async delete(key: string): Promise<void> {
+    await this.redis.del(key);
+  }
+}
