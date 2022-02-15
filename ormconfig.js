7bad3f64ec6a33d7f41cde47afe4e6b8bb5a5d9b
Thobias P Silva <thobiaspsilva@yahoo.com.br>
2022-02-15 00:27:41 -0300

    configura o jest

--- ormconfig.js
+++ ormconfig.js
@@ -1,21 +1,38 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const eProducao = process.env.NODE_ENV?.toLocaleLowerCase() === 'prod';
+let config = {};
 
-const pastaRaiz = eProducao ? 'dist' : 'src';
+if (process.env.NODE_ENV?.toLocaleLowerCase() === "test") {
+  config = {
+    type: "sqlite",
+    url: "./testdb.sqlite",
+    synchronize: false,
+    entities: ["src/core/infra/database/entities/**/*"],
+    migrations: ["tests/core/infra/database/migrations/**/*"],
+    cli: {
+      entitiesDir: "src/core/infra/database/entities",
+      migrationsDir: "tests/core/infra/database/migrations",
+    },
+  };
+} else {
+  const eProducao = process.env.NODE_ENV?.toLocaleLowerCase() === "prod";
+  const pastaRaiz = eProducao ? "dist" : "src";
 
-module.exports = {
+  config = {
     type: "postgres",
     url: process.env.DATABASE_URL,
     synchronize: false,
     logging: false,
-    entities: [pastaRaiz + '/core/infra/database/entities/**/*'],
-    migrations: [pastaRaiz + '/core/infra/database/migrations/**/*'],
+    entities: [pastaRaiz + "/core/infra/database/entities/**/*"],
+    migrations: [pastaRaiz + "/core/infra/database/migrations/**/*"],
     cli: {
-        entitiesDir: 'src/core/infra/database/entities',
-        migrationsDir: 'src/core/infra/database/migrations',
+      entitiesDir: "src/core/infra/database/entities",
+      migrationsDir: "src/core/infra/database/migrations",
     },
     ssl: {
-        rejectUnauthorized: false
-    }
-};
+      rejectUnauthorized: false,
+    },
+  };
+}
+
+module.exports = config;
