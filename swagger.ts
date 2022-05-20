08d4a226be2a21a3298f10dc845bd3c560b5935d
Thobias P Silva <thobiaspsilva@yahoo.com.br>
2022-05-19 22:43:55 -0300

    Cria documentacao

--- swagger.ts
+++ swagger.ts
@@ -10,10 +10,44 @@ export default {
                 type: 'object',
                 properties: {
                     login: {
-                        type: 'string',            
+                        type: 'string',
                     },
                     password: {
-                        type: 'string',                  
+                        type: 'string',
+                    },
+                },
+            },
+            createMessageParams: {
+                type: 'object',
+                properties: {
+                    title: {
+                        type: 'string',
+                    },
+                    detail: {
+                        type: 'string',
+                    },
+                },
+            },
+            message: {
+                type: 'object',
+                properties: {
+                    uid: {
+                        type: 'string',
+                    },
+                    title: {
+                        type: 'string',
+                    },
+                    detail: {
+                        type: 'string',
+                    },
+                    userUid: {
+                        type: 'string',
+                    },
+                    createdAt: {
+                        type: 'string',
+                    },
+                    updatedAt: {
+                        type: 'string',
                     },
                 },
             },
@@ -37,7 +71,208 @@ export default {
                     },
                 },
                 responses: {
-                    description: 'ds',
+                    200: {
+                        description: 'Ok',
+                        schema: {
+                            type: 'object',
+                            properties: {
+                                $ref: '#/components/schemas/message',
+                            },
+                        },
+                    },
+                    400: {  
+                        description: "Bad Request",                       
+                    },
+                    500: {
+                        description: 'Server Error',
+                    },
+                },
+            },
+        },
+        '/register': {
+            summary: 'Register route',
+            post: {
+                summary: 'Register route',
+                description: 'Route for create account',
+                tags: ['Auth'],
+                requestBody: {
+                    content: {
+                        'application/json': {
+                            schema: {
+                                $ref: '#/components/schemas/auth',
+                            },
+                        },
+                        required: true,
+                    },
+                },
+                responses: {
+                    200: {
+                        description: 'Ok',
+                        schema: {
+                            type: 'object',
+                            properties: {
+                                $ref: '#/components/schemas/message',
+                            },
+                        },
+                    },
+                    400: {  
+                        description: "Bad Request",                       
+                    },
+                    500: {
+                        description: 'Server Error',
+                    },
+                },
+            },
+        },
+        '/messages': {
+            summary: 'Get Messages route',
+            get: {
+                summary: 'Get Messages route',
+                description: 'Route for get messages',
+                tags: ['Message'],
+                parameters: [
+                    {
+                        name: "authorization",
+                        in: "header",
+                        description: "userUid",
+                        required: true,
+                        type: "string",           
+                    }
+                ],
+                responses: {
+                    200: {
+                        description: 'Ok',
+                        schema: {
+                            type: 'array',
+                            items: {
+                                $ref: '#/components/schemas/message',
+                            },
+                        },
+                    },
+                    500: {
+                        description: 'Server Error',
+                    },
+                },
+            },
+            post: {
+                summary: 'Create Message route',
+                description: 'Route for create message',
+                tags: ['Message'],
+                parameters: [
+                    {
+                        name: "authorization",
+                        in: "header",
+                        description: "userUid",
+                        required: true,
+                        type: "string",           
+                    }
+                ],
+                requestBody: {
+                    content: {
+                        'application/json': {
+                            schema: {
+                                $ref: '#/components/schemas/createMessageParams',
+                            },
+                        },
+                        required: true,
+                    },
+                },
+                responses: {
+                    200: {
+                        description: 'Ok',
+                        schema: {
+                            type: 'object',
+                            properties: {
+                                $ref: '#/components/schemas/message',
+                            },
+                        },
+                    },
+                    400: {  
+                        description: "Bad Request",                       
+                    },
+                    500: {
+                        description: 'Server Error',
+                    },
+                },
+            },
+        },
+        '/messages/:uid': {
+            summary: 'Update Message route',
+            put: {
+                summary: 'Update Message route',
+                description: 'Route for update message',
+                tags: ['Message'],
+                parameters: [
+                    {
+                        name: "uid",
+                        in: "path",
+                        description: "uid of message for updated",
+                        required: true,
+                        type: "string",           
+                    },
+                    {
+                        name: "authorization",
+                        in: "header",
+                        description: "userUid",
+                        required: true,
+                        type: "string",           
+                    }
+                ],
+                responses: {
+                    200: {
+                        description: 'Ok',
+                        schema: {
+                            type: 'object',
+                            properties: {
+                                $ref: '#/components/schemas/message',
+                            },
+                        },
+                        
+                    }, 
+                    400: {
+                        description: "Bad Request",
+                    },
+                    500: {
+                        description: 'Server Error',
+                    },
+                },
+            },
+            delete: {
+                summary: 'Delete Message route',
+                description: 'Route for delete message',
+                tags: ['Message'],
+                parameters: [
+                    {
+                        name: "uid",
+                        in: "path",
+                        description: "uid of message for delete",
+                        required: true,
+                        type: "string",           
+                    },
+                    {
+                        name: "authorization",
+                        in: "header",
+                        description: "userUid",
+                        required: true,
+                        type: "string",           
+                    }
+                ],
+                responses: {
+                    200: {
+                        description: "Ok",
+                        schema: {
+                            type: 'object',
+                            properties: {
+                                $ref: '#/components/schemas/message',
+                            },
+                        },
+                    },
+                    400: {
+                        description: "Bad Request",
+                    },
+                    500: {
+                        description: "Server Error",
+                    }
                 },
             },
         },
